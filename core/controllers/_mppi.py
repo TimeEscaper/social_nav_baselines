016825a084b81867f1a34bac9b51e994eeb48f15
Timur Akhtyamov <rentgeny05@gmail.com>
2023-02-25 16:38:21 +0300

    MPPI controller added

---
+++ core/controllers/_mppi.py
@@ -0,0 +1,232 @@
+import numpy as np
+import torch
+
+from ._controller import AbstractController
+from core.predictors import AbstractPredictor, NeuralPredictor, ConstantVelocityPredictor
+from core.predictors import PedestrianTracker
+import do_mpc
+import casadi
+from pytorch_mppi import MPPI
+from typing import List, Dict, Optional
+
+
+class _StageControlCost:
+
+    def __init__(self, R: np.ndarray, device: str):
+        self._R = torch.tensor(R).float().to(device)
+
+    def __call__(self, state: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
+        cost = torch.einsum("ni,ji->nj", action, self._R)
+        cost = torch.einsum("ni,ni->n", action, cost)
+        return cost
+
+
+class _FullTrajectoryCost:
+
+    def __init__(self, Q: float, stage_weight: float, r_robot: float,
+                 r_ped: float, device: str):
+        self._Q = Q
+        self._stage_weight: stage_weight
+        self._r_robot = r_robot
+        self._r_ped = r_ped
+        self._device = device
+        self._goal = None
+        self._initial_state = None
+        self._peds_traj = None
+        self._peds_covs = None
+
+    def update(self, goal: np.ndarray, initial_state: np.ndarray,
+               ped_traj: Optional[torch.Tensor], ped_covs: Optional[torch.Tensor]):
+        self._goal = torch.tensor(goal).float().to(self._device)
+        self._initial_state = torch.tensor(initial_state).float().to(self._device)
+        if ped_traj is not None and ped_covs is not None:
+            self._peds_traj = torch.tensor(ped_traj).float().to(self._device)
+            self._peds_covs = torch.tensor(ped_covs).float().to(self._device)
+        else:
+            self._peds_traj = None
+            self._peds_covs = None
+
+    def __call__(self, states: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
+        cost = self._calculate_goal_cost(states)
+        cost = cost + self._calculate_social_cost(states)
+        return cost
+
+    def _calculate_goal_cost(self, states: torch.Tensor) -> torch.Tensor:
+        final_state = states[:, :, -1, :]
+        initial_dist = torch.linalg.norm(self._initial_state - self._goal)
+        final_dist = torch.linalg.norm(states[:, :, -1, :] - self._goal, dim=-1)
+        cost = self._Q * (final_dist / initial_dist) ** 2
+        return cost
+
+    def _calculate_social_cost(self, states: torch.Tensor) -> torch.Tensor:
+        return 0.
+
+
+class _UnicycleDynamics:
+
+    def __init__(self, dt: float):
+        self._dt = dt
+
+    def __call__(self, state: torch.Tensor, action: torch.Tensor):
+        new_state = torch.zeros_like(state)
+        new_state[:, 0] = state[:, 0] + action[:, 0] * torch.cos(state[:, 2]) * self._dt
+        new_state[:, 1] = state[:, 1] + action[:, 0] * torch.sin(state[:, 2]) * self._dt
+        new_state[:, 2] = state[:, 2] + action[:, 1] * self._dt
+        return new_state
+
+    def rollout_dynamics(self, start_state: np.ndarray, actions: np.ndarray) -> np.ndarray:
+        traj = np.zeros((actions.shape[0], 3))
+        for i in range(actions.shape[0]):
+            if i != 0:
+                previous_state = traj[i - 1]
+            else:
+                previous_state = start_state
+            traj[i, 0] = previous_state[0] + actions[i, 0] * np.cos(previous_state[2]) * self._dt
+            traj[i, 1] = previous_state[1] + actions[i, 0] * np.sin(previous_state[2]) * self._dt
+            traj[i, 2] = previous_state[2] + actions[i, 1] * self._dt
+        return traj
+
+
+class MPPIController(AbstractController):
+
+    def __init__(self,
+                 init_state: np.ndarray,
+                 goal: np.ndarray,
+                 horizon: int,
+                 dt: float,
+                 model_type: str,
+                 total_peds: int,
+                 Q: np.ndarray,
+                 R: np.ndarray,
+                 W: float,
+                 noise_sigma: np.ndarray,
+                 lb: List[float],
+                 ub: List[float],
+                 r_rob: float,
+                 r_ped: float,
+                 pedestrian_tracker: PedestrianTracker,
+                 max_ghost_tracking_time: int,
+                 cost_function: str,
+                 num_samples: int = 50,
+                 lambda_: float = 1.,
+                 device: str = "cpu") -> None:
+        """Initiazition of the class instance
+
+        Args:
+            init_state (np.ndarray): Initial state of the system
+            goal (np.ndarray): Goal of the system
+            horizon (int): Prediction horizon of the controller, [steps]
+            dt (float): Time delta, [s]
+            model_type (str): Type of the model, ["unicycle", "unicycle_double_integrator"]
+            total_peds (int): Amount of pedestrians in the system, >= 0
+            Q (np.ndarray): State weighted matrix, [state_dim * state_dim]
+            R (np.ndarray): Control weighted matrix, [control_dim * control_dim]
+            lb (List[float]): Lower boundaries for system states or/and controls
+            ub (List[float]): Upper boundaries for system states or/and controls
+            r_rob (float): Robot radius, [m]
+            r_ped (float): Pedestrian radius, [m]
+            constraint_value (float): Minimal safe distance between robot and pedestrian, [m]
+            predictor (AbstractPredictor): Predictor, [Constant Velocity Predictor, Neural Predictor]
+            # TODO
+
+        Returns:
+            _type_: None
+        """
+        super().__init__(init_state,
+                         self.get_ref_direction(init_state,
+                                                goal),
+                         horizon,
+                         dt,
+                         [],
+                         max_ghost_tracking_time)
+        self._ped_tracker = pedestrian_tracker
+        self._total_peds = total_peds
+        self._full_traj_cost = _FullTrajectoryCost(Q=Q,
+                                                   stage_weight=W,
+                                                   r_robot=r_rob,
+                                                   r_ped=r_ped,
+                                                   device=device)
+
+        if model_type == "unicycle":
+            dynamics = _UnicycleDynamics(dt=dt)
+        else:
+            raise ValueError("Only unicycle model for now implemented for MPPI")
+        self._dynamics = dynamics
+        stage_cost = _StageControlCost(R=R, device=device)
+
+        if len(noise_sigma.shape) == 1:
+            noise_sigma = np.array([[noise_sigma[0], 0.],
+                                    [0., noise_sigma[1]]])
+        elif len(noise_sigma.shape) == 2:
+            assert noise_sigma[0, 1] == noise_sigma[1, 0], "noise_sigma must be proper covariance matrix"
+        else:
+            raise ValueError("noise_sigma must be a vector (with sigmas for each action dim) or proper covariancew matrix")
+
+        self._mppi = MPPI(dynamics=dynamics,
+                          running_cost=stage_cost,
+                          nx=3,
+                          noise_sigma=torch.tensor(noise_sigma).float().to(device),
+                          num_samples=num_samples,
+                          horizon=horizon+1,
+                          device=device,
+                          terminal_state_cost=self._full_traj_cost,
+                          lambda_=lambda_,
+                          u_min=torch.tensor(lb, dtype=torch.double, device=device),
+                          u_max=torch.tensor(ub, dtype=torch.double, device=device),
+                          u_per_command=horizon+1)
+        self._planned_traj = np.zeros((horizon+1, 3))
+
+    def predict(self,
+                observation: Dict[int, np.ndarray]) -> np.ndarray:
+        """Method predicts pedestrian trajectories with specified predictor
+
+        Args:
+            ground_truth_pedestrians_state (np.ndarray): Current state of the pedestrians, [2-d numpy array]
+        """
+        self._ped_tracker.update(observation)
+        tracked_predictions = self._ped_tracker.get_predictions()
+
+        predicted_trajectories = np.tile(np.array([1000., 1000., 0., 0.]), (self._horizon + 1, self._total_peds, 1))
+        predicted_covs = np.tile(np.array([[0.01, 0.0], [0., 0.01]]), (self._horizon + 1, self._total_peds, 1, 1))
+        for k in tracked_predictions.keys():
+            predicted_trajectories[:, k, :2] = tracked_predictions[k][0]
+            predicted_covs[:, k, :] = tracked_predictions[k][1]
+
+        self._predicted_pedestrians_trajectories = predicted_trajectories
+        self._predicted_pedestrians_covariances = predicted_covs
+
+        # For MPPI, number of pedestrians is not a problem unlike for Casadi, so we handle them separately
+        if len(tracked_predictions) == 0:
+            return None, None
+        tracked_trajs = np.stack([v[0] for v in tracked_predictions.values()], axis=1)
+        tracked_covs = np.stack([v[1] for v in tracked_predictions.values()], axis=1)
+
+
+        return tracked_trajs, tracked_covs
+
+    def make_step(self,
+                  state: np.ndarray,
+                  observation: Dict[int, np.ndarray]) -> np.ndarray:
+        peds_traj, peds_cov = self.predict(observation)
+        self._full_traj_cost.update(self._goal, state, peds_traj, peds_cov)
+        controls = self._mppi.command(state).clone().detach().cpu().numpy()
+        self._planned_traj = self._dynamics.rollout_dynamics(state, controls)[:, :2]
+        control = controls[0]
+        return control, self._predicted_pedestrians_trajectories, self._predicted_pedestrians_covariances
+
+    def set_new_goal(self,
+                     current_state: np.ndarray,
+                     new_goal: np.ndarray) -> None:
+        super().set_new_goal(current_state,
+                             new_goal)
+
+    def get_predicted_robot_trajectory(self) -> List[float]:
+        return self._planned_traj
+
+    @property
+    def init_state(self) -> np.ndarray:
+        return self._init_state
+
+    @property
+    def predicted_pedestrians_trajectories(self) -> np.ndarray:
+        return self._predicted_pedestrians_trajectories
---
+++ examples/mppi.py
@@ -0,0 +1,176 @@
+from core.controllers import MPPIController
+from core.predictors import ConstantVelocityPredictor, NeuralPredictor, PedestrianTracker
+from core.utils import create_sim
+from core.visualizer import Visualizer
+from core.statistics import Statistics
+import numpy as np
+import fire
+import yaml
+import pygame
+import pathlib
+
+pathlib.Path(r"results").mkdir(parents=True, exist_ok=True)
+
+DEFAULT_SCENE_CONFIG_PATH = r"configs/scenes/random/1_pedestrian.yaml"
+DEFAULT_CONTROLLER_CONFIG_PATH = r"configs/controllers/mppi.yaml"
+DEFAULT_RESULT_PATH = r"results/mppi.gif"
+
+
+def main(scene_config_path: str = DEFAULT_SCENE_CONFIG_PATH,
+         controller_config_path: str = DEFAULT_CONTROLLER_CONFIG_PATH,
+         result_path: str = DEFAULT_RESULT_PATH) -> Statistics:
+    # Initialization
+    with open(scene_config_path) as f:
+        scene_config = yaml.safe_load(f)
+
+    with open(controller_config_path) as f:
+        controller_config = yaml.safe_load(f)
+
+    config = {}
+    config.update(scene_config)
+    config.update(controller_config)
+
+    simulator, renderer = create_sim(np.array(config["init_state"]),
+                                     config["model_type"],
+                                     config["total_peds"],
+                                     config["is_robot_visible"],
+                                     config["ped_detc_range"],
+                                     np.deg2rad(config["ped_detc_fov"]),
+                                     config["misdetection_prob"],
+                                     config["dt"] / 10,
+                                     config["waypoint_tracker"],
+                                     config["pedestrians_init_states"],
+                                     config["pedestrians_goals"],
+                                     config["ped_model"])
+    renderer.initialize()
+    if config["ped_predictor"] == "constant_velocity":
+        if config["total_peds"] > 0:
+            predictor = ConstantVelocityPredictor(config["dt"],
+                                                  config["total_peds"],
+                                                  config["horizon"])
+        elif config["total_peds"] == 0:
+            predictor = ConstantVelocityPredictor(config["dt"],
+                                                  1,
+                                                  config["horizon"])
+    elif config["ped_predictor"] == "neural":
+        if config["total_peds"] > 0:
+            predictor = NeuralPredictor(total_peds=config["total_peds"],
+                                        horizon=config["horizon"],
+                                        device=config["device"])
+        elif config["total_peds"] == 0:
+            predictor = NeuralPredictor(total_peds=1,
+                                        horizon=config["horizon"],
+                                        device=config["device"])
+
+    controller = MPPIController(init_state=np.array(config["init_state"]),
+                                goal=np.array(config["goal"]),
+                                horizon=config["horizon"],
+                                dt=config["dt"],
+                                model_type=config["model_type"],
+                                total_peds=config["total_peds"],
+                                Q=config["Q"],
+                                R=np.array(config["R"]),
+                                W=config["W"],
+                                noise_sigma=np.array(config["noise_sigma"]),
+                                lb=config["lb"],
+                                ub=config["ub"],
+                                r_rob=config["r_rob"],
+                                r_ped=config["r_ped"],
+                                pedestrian_tracker=PedestrianTracker(predictor, config["max_ghost_tracking_time"], False),
+                                max_ghost_tracking_time=config["max_ghost_tracking_time"],
+                                cost_function=config["cost_function"],
+                                num_samples=config["num_samples"],
+                                lambda_=config["lambda_"],
+                                device=config["device"]
+                                )
+    visualizer = Visualizer(config["total_peds"],
+                            renderer)
+    visualizer.visualize_goal(config["goal"])
+
+    statistics = Statistics(simulator,
+                            scene_config,
+                            controller_config)
+
+    # Loop
+    simulator.step()
+    hold_time = simulator.sim_dt
+    state = config["init_state"]
+    control = np.array([0, 0])
+
+    while True:
+        renderer.render()
+        if hold_time >= controller.dt:
+            error = np.linalg.norm(controller.goal[:2] - state[:2])
+            if error >= config["tollerance_error"]:
+                state = simulator.current_state.world.robot.state
+
+                detected_peds_keys = simulator.current_state.sensors["pedestrian_detector"].reading.pedestrians.keys()
+                ground_truth_pedestrians_state = np.concatenate([simulator.current_state.world.pedestrians.poses[:, :2],
+                                                                 simulator.current_state.world.pedestrians.velocities[:,
+                                                                 :2]], axis=1)
+                observation = {k: ground_truth_pedestrians_state[k, :] for k in detected_peds_keys}
+
+                visualizer.append_ground_truth_robot_state(state)
+                visualizer.append_ground_truth_pedestrians_pose(simulator.current_state.world.pedestrians.poses[:, :2])
+
+                control, predicted_pedestrians_trajectories, predicted_pedestrians_covariances = controller.make_step(
+                    state,
+                    observation)
+
+                # visualizer.append_ground_truth_robot_state(state)
+                # if config["total_peds"] > 0:
+                #     detected_pedestrian_indices = simulator.current_state.sensors['pedestrian_detector'].reading.pedestrians.keys()
+                #     undetected_pedestrian_indices = list(set(range(config["total_peds"])) - set(detected_pedestrian_indices))
+                #     ground_truth_pedestrians_state = np.concatenate([simulator.current_state.world.pedestrians.poses[:, :2],
+                #                                                      simulator.current_state.world.pedestrians.velocities[:, :2]], axis=1)
+                #     ground_truth_pedestrians_state[undetected_pedestrian_indices] = config["state_dummy_ped"]
+                #     controller.previously_detected_pedestrians |= set(detected_pedestrian_indices)
+                #     pedestrians_ghosts_states = ground_truth_pedestrians_state
+                #     if undetected_pedestrian_indices:
+                #         pedestrians_ghosts_states = controller.get_pedestrains_ghosts_states(ground_truth_pedestrians_state,
+                #                                                                              undetected_pedestrian_indices)
+                #
+                #     visualizer.append_ground_truth_pedestrians_pose(simulator.current_state.world.pedestrians.poses[:, :2])
+                # elif config["total_peds"] == 0:
+                #     pedestrians_ghosts_states = np.array([config["state_dummy_ped"]])
+                # control, predicted_pedestrians_trajectories, predicted_pedestrians_covariances = controller.make_step(state,
+                #                                                                                                       pedestrians_ghosts_states)
+                visualizer.append_predicted_pedestrians_trajectories(predicted_pedestrians_trajectories[:, :, :2])
+                # visualizer.visualize_predicted_pedestrians_trajectories(predicted_pedestrians_trajectories[:, :, :2])
+
+                visualizer.visualize_predicted_pedestrians_trajectory_with_covariances(
+                    predicted_pedestrians_trajectories[:, :, :2], predicted_pedestrians_covariances)
+                predicted_robot_trajectory = controller.get_predicted_robot_trajectory()
+                visualizer.append_predicted_robot_trajectory(predicted_robot_trajectory)
+                visualizer.visualize_predicted_robot_trajectory(predicted_robot_trajectory)
+                visualizer.append_predicted_pedestrians_covariances(predicted_pedestrians_covariances)
+                hold_time = 0.
+            else:
+                simulator.step(np.array([0, 0]))
+                print("The goal", *["{0:0.2f}".format(i) for i in controller.goal], "was reached!")
+                input_value = input("Enter new goal in format '0 0 0' or type 'exit' to exit: ")
+                if input_value == "exit":
+                    break
+                new_goal = np.array(list(map(int, input_value.split())))
+                visualizer.visualize_goal(new_goal)
+                controller.set_new_goal(state,
+                                        new_goal)
+        simulator.step(control)
+        statistics.track_collisions()
+        statistics.track_simulation_ticks()
+        hold_time += simulator.sim_dt
+        # Terminate the simulation anytime by pressing ESC
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+    pygame.quit()
+
+    visualizer.make_animation(f"MPC",
+                              result_path,
+                              config)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
