43f9e3abdb69bb4dc1aa8a5bfbfca0258631b039
Aleksandr Kashirin <kashirin.aleks@ya.ru>
2023-03-31 16:05:51 +0300

    Automation of experiments in progress

--- evaluation/scripts/generate_scenes.py
+++ evaluation/scripts/generate_scenes.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import pathlib
 import matplotlib.pyplot as plt
+from typing import List
 
 PEDESTRIAN_RANGE = [3, 4, 5, 6]
 TOTAL_SCENARIOS = 100
@@ -10,14 +11,9 @@ ROBOT_VISION_RANGE = 5
 INFLATION_RADIUS = 1.0 # robot_radius + pedestrian_radius + safe_distances
 BORDERS_X = [-5, 5]
 BORDERS_Y = [-5, 5]
-
-SEED = 42
-np.random.seed(42)
-random.seed(42)
-
 DEFAULT_COLOR_HEX_PALETTE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
-with open(r"evaluation/scenes/template_scene_config.yaml") as f:
+with open(r"evaluation/configs/scenes/scene_config.yaml") as f:
     config = yaml.safe_load(f)
 
 def visualize_scenario(pedestrians_initial_positions: np.ndarray, 
@@ -32,8 +28,6 @@ def visualize_scenario(pedestrians_initial_positions: np.ndarray,
         robot_initial_position (np.ndarray): robot initial position
         robot_goal_position (np.ndarray): robot goal position
     """
-    #print(pedestrians_initial_positions)
-    #print(pedestrians_goal_positions)
     fig, ax = plt.subplots(figsize=[16, 16], facecolor='white')
 
     total_peds = pedestrians_goal_positions.shape[0]
@@ -69,7 +63,11 @@ def is_point_belongs_circle(point: np.ndarray,
     return dist <= circle_radius
 
 def sample_robot_final_position(robot_initial_position: np.ndarray,
-                                pedestrians_positions: np.ndarray) -> np.ndarray:
+                                pedestrians_positions: np.ndarray,
+                                robot_vision_range: float = ROBOT_VISION_RANGE,
+                                borders_x: List[float] = BORDERS_X,
+                                borders_y: List[float] = BORDERS_Y,
+                                inflation_radius: float = INFLATION_RADIUS) -> np.ndarray:
     """Sample robot final position in vision radius
 
     Args:
@@ -85,7 +83,7 @@ def sample_robot_final_position(robot_initial_position: np.ndarray,
         cnt += 1
         # Polar
         phi = np.random.uniform(0, 2 * np.pi)
-        r = np.random.uniform(0.5 * ROBOT_VISION_RANGE, ROBOT_VISION_RANGE)
+        r = np.random.uniform(0.5 * robot_vision_range, robot_vision_range)
         # Cartesian
         delta_x = r * np.cos(phi)
         delta_y = r * np.sin(phi)
@@ -93,8 +91,8 @@ def sample_robot_final_position(robot_initial_position: np.ndarray,
                                         robot_initial_position[1] + delta_y, 
                                         0])
         # Check if position is within the borders
-        if BORDERS_X[0] <= robot_goal_position[0] <= BORDERS_X[1] and \
-           BORDERS_Y[0] <= robot_goal_position[1] <= BORDERS_Y[1]:
+        if borders_x[0] <= robot_goal_position[0] <= borders_x[1] and \
+           borders_y[0] <= robot_goal_position[1] <= borders_y[1]:
             pass
         else:
             continue
@@ -102,7 +100,7 @@ def sample_robot_final_position(robot_initial_position: np.ndarray,
         for position in pedestrians_positions:
             resample_flag = is_point_belongs_circle(robot_goal_position[:2],
                                                     position[:2],
-                                                    INFLATION_RADIUS*2.5)
+                                                    inflation_radius*2.5)
             if resample_flag:
                 break
         if not resample_flag:
@@ -111,11 +109,17 @@ def sample_robot_final_position(robot_initial_position: np.ndarray,
         print("Generation limit was reached!")
     return robot_goal_position
 
-def generate_circular_scenarios(vizualization: bool = False,
+def generate_circular_scenarios(pedestrian_range: List[int],
+                                total_scenarios: int,
+                                robot_vision_range: float,
+                                inflation_radius: float,
+                                borders_x: List[int],
+                                borders_y: List[int],
+                                visualization: bool = False,
                                 save_config: bool = True) -> None:
     """ Circular crossing scenario
     """
-    pathlib.Path("evaluation/scenes/circular_crossing").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("evaluation/configs/scenes/circular_crossing").mkdir(parents=True, exist_ok=True)
 
     min_circle_rad = 3.8
     max_circle_rad = 4.2
@@ -131,8 +135,8 @@ def generate_circular_scenarios(vizualization: bool = False,
             x = r * np.cos(phi)
             y = r * np.sin(phi)
             if robot:
-                x = np.random.uniform(*BORDERS_X)
-                y = np.random.uniform(*BORDERS_Y)
+                x = np.random.uniform(*borders_x)
+                y = np.random.uniform(*borders_y)
                 phi = np.random.uniform(-np.pi, np.pi)
             position = np.array([x, y, phi + np.pi])
             # Check if position i is not intersect with pedestrians
@@ -141,16 +145,16 @@ def generate_circular_scenarios(vizualization: bool = False,
             for pos in saved_positions:
                 resample_flag = is_point_belongs_circle(position[:2],
                                                         pos[:2],
-                                                        INFLATION_RADIUS)
+                                                        inflation_radius)
                 if resample_flag:
                     break
             if not resample_flag:
                 break
         return position
 
-    for total_peds in PEDESTRIAN_RANGE:
-        pathlib.Path(f"evaluation/scenes/circular_crossing/{total_peds}").mkdir(parents=True, exist_ok=True)
-        for id in range(TOTAL_SCENARIOS):
+    for total_peds in pedestrian_range:
+        pathlib.Path(f"evaluation/configs/scenes/circular_crossing/{total_peds}").mkdir(parents=True, exist_ok=True)
+        for id in range(total_scenarios):
             pedestrians_initial_positions = np.zeros([total_peds, 3])
             pedestrians_goal_positions = np.zeros([total_peds, 3])
             # Sample pedestrians
@@ -167,8 +171,12 @@ def generate_circular_scenarios(vizualization: bool = False,
             robot_initial_position = position
             robot_goal_position = sample_robot_final_position(robot_initial_position,
                                                               np.vstack([pedestrians_initial_positions,
-                                                                         pedestrians_goal_positions]))
-            if vizualization:
+                                                                         pedestrians_goal_positions]),
+                                                            robot_vision_range=robot_vision_range,
+                                                            borders_x=borders_x,
+                                                            borders_y=borders_y,
+                                                            inflation_radius=inflation_radius)
+            if visualization:
                 # Plot points
                 visualize_scenario(pedestrians_initial_positions,
                                 pedestrians_goal_positions,
@@ -182,23 +190,29 @@ def generate_circular_scenarios(vizualization: bool = False,
                 config["pedestrians_init_states"] = pedestrians_initial_positions.tolist()
                 config["pedestrians_goals"] = pedestrians_goal_positions[:, :2].reshape(total_peds, 1, 2).tolist()
 
-                with open(fr'evaluation/scenes/circular_crossing/{total_peds}/{id}.yaml', 'w') as outfile:
+                with open(fr'evaluation/configs/scenes/circular_crossing/{total_peds}/{id}.yaml', 'w') as outfile:
                     yaml.dump(config, outfile)
     
-def generate_random_scenarios(vizualization: bool = False,
+def generate_random_scenarios(pedestrian_range: List[int],
+                                total_scenarios: int,
+                                robot_vision_range: float,
+                                inflation_radius: float,
+                                borders_x: List[int],
+                                borders_y: List[int],
+                                visualization: bool = False,
                                 save_config: bool = True) -> None:
     """ Random crossing scenario
     """
-    pathlib.Path("evaluation/scenes/random_crossing").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("evaluation/configs/scenes/random_crossing").mkdir(parents=True, exist_ok=True)
 
     def generate_position(pedestrians_initial_positions: np.ndarray,
                           pedestrians_goal_positions: np.ndarray,
                           robot: bool = False) -> np.ndarray:
-        inflation_radius = INFLATION_RADIUS*2 if robot else INFLATION_RADIUS
+        inf_rad = inflation_radius*2 if robot else inflation_radius
         while True:
             # Cartesian
-            x = np.random.uniform(*BORDERS_X)
-            y = np.random.uniform(*BORDERS_Y)
+            x = np.random.uniform(*borders_x)
+            y = np.random.uniform(*borders_y)
             phi = np.random.uniform(-np.pi, np.pi)
             position = np.array([x, y, phi])
             # Check if position i is not intersect with pedestrians
@@ -207,16 +221,16 @@ def generate_random_scenarios(vizualization: bool = False,
             for pos in saved_positions: 
                 resample_flag = is_point_belongs_circle(position[:2],
                                                         pos[:2],
-                                                        inflation_radius)
+                                                        inf_rad)
                 if resample_flag:
                     break
             if not resample_flag:
                 break
         return position
 
-    for total_peds in PEDESTRIAN_RANGE:
-        pathlib.Path(f"evaluation/scenes/random_crossing/{total_peds}").mkdir(parents=True, exist_ok=True)
-        for id in range(TOTAL_SCENARIOS):
+    for total_peds in pedestrian_range:
+        pathlib.Path(f"evaluation/configs/scenes/random_crossing/{total_peds}").mkdir(parents=True, exist_ok=True)
+        for id in range(total_scenarios):
             pedestrians_initial_positions = np.zeros([total_peds, 3])
             pedestrians_goal_positions = np.zeros([total_peds, 3])
             # Sample pedestrians
@@ -235,8 +249,12 @@ def generate_random_scenarios(vizualization: bool = False,
             robot_initial_position = position
             robot_goal_position = sample_robot_final_position(robot_initial_position,
                                                               np.vstack([pedestrians_initial_positions,
-                                                                         pedestrians_goal_positions]))
-            if vizualization:
+                                                                         pedestrians_goal_positions]),
+                                                            robot_vision_range=robot_vision_range,
+                                                            borders_x=borders_x,
+                                                            borders_y=borders_y,
+                                                            inflation_radius=inflation_radius)
+            if visualization:
                 # Plot points
                 visualize_scenario(pedestrians_initial_positions,
                                 pedestrians_goal_positions,
@@ -250,14 +268,20 @@ def generate_random_scenarios(vizualization: bool = False,
                 config["pedestrians_init_states"] = pedestrians_initial_positions.tolist()
                 config["pedestrians_goals"] = pedestrians_goal_positions[:, :2].reshape(total_peds, 1, 2).tolist()
 
-                with open(fr'evaluation/scenes/random_crossing/{total_peds}/{id}.yaml', 'w') as outfile:
+                with open(fr'evaluation/configs/scenes/random_crossing/{total_peds}/{id}.yaml', 'w') as outfile:
                     yaml.dump(config, outfile)
                 
-def generate_parallel_scenarios(vizualization: bool = False,
+def generate_parallel_scenarios(pedestrian_range: List[int],
+                                total_scenarios: int,
+                                robot_vision_range: float,
+                                inflation_radius: float,
+                                borders_x: List[int],
+                                borders_y: List[int],
+                                visualization: bool = False,
                                   save_config: bool = True)-> None:
     """ Parallel crossing scenario
     """
-    pathlib.Path("evaluation/scenes/parallel_crossing").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("evaluation/configs/scenes/parallel_crossing").mkdir(parents=True, exist_ok=True)
 
     range_x = [-4, -3]
     range_y = [-4, 4]
@@ -279,16 +303,16 @@ def generate_parallel_scenarios(vizualization: bool = False,
             for pos in saved_positions:
                 resample_flag = is_point_belongs_circle(position[:2],
                                                         pos[:2],
-                                                        INFLATION_RADIUS)
+                                                        inflation_radius)
                 if resample_flag:
                     break
             if not resample_flag:
                 break
         return position
 
-    for total_peds in PEDESTRIAN_RANGE:
-        pathlib.Path(f"evaluation/scenes/parallel_crossing/{total_peds}").mkdir(parents=True, exist_ok=True)
-        for id in range(TOTAL_SCENARIOS):
+    for total_peds in pedestrian_range:
+        pathlib.Path(f"evaluation/configs/scenes/parallel_crossing/{total_peds}").mkdir(parents=True, exist_ok=True)
+        for id in range(total_scenarios):
             pedestrians_initial_positions = np.zeros([total_peds, 3])
             pedestrians_goal_positions = np.zeros([total_peds, 3])
             # Sample pedestrians
@@ -306,8 +330,12 @@ def generate_parallel_scenarios(vizualization: bool = False,
             robot_initial_position = position
             robot_goal_position = sample_robot_final_position(robot_initial_position,
                                                               np.vstack([pedestrians_initial_positions,
-                                                                         pedestrians_goal_positions]))
-            if vizualization:
+                                                                         pedestrians_goal_positions]),
+                                                            robot_vision_range=robot_vision_range,
+                                                            borders_x=borders_x,
+                                                            borders_y=borders_y,
+                                                            inflation_radius=inflation_radius)
+            if visualization:
                 # Plot points
                 visualize_scenario(pedestrians_initial_positions,
                                 pedestrians_goal_positions,
@@ -321,14 +349,52 @@ def generate_parallel_scenarios(vizualization: bool = False,
                 config["pedestrians_init_states"] = pedestrians_initial_positions.tolist()
                 config["pedestrians_goals"] = pedestrians_goal_positions[:, :2].reshape(total_peds, 1, 2).tolist()
 
-                with open(fr'evaluation/scenes/parallel_crossing/{total_peds}/{id}.yaml', 'w') as outfile:
+                with open(fr'evaluation/configs/scenes/parallel_crossing/{total_peds}/{id}.yaml', 'w') as outfile:
                     yaml.dump(config, outfile)
 
-def main():
-    generate_circular_scenarios(False)
-    generate_random_scenarios(False)
-    generate_parallel_scenarios(False)
+
+
+def generate_scenes(pedestrian_range: List[int] = PEDESTRIAN_RANGE,
+                    total_scenarios: int = TOTAL_SCENARIOS,
+                    robot_vision_range: float = ROBOT_VISION_RANGE,
+                    inflation_radius: float = INFLATION_RADIUS,
+                    borders_x: List[int] = BORDERS_X,
+                    borders_y: List[int] = BORDERS_Y,
+                    seed: int = 42) -> None:
+    
+    # Set random seed
+    np.random.seed(seed)
+    random.seed(seed)
+
+    generate_circular_scenarios(pedestrian_range,
+                                total_scenarios,
+                                robot_vision_range,
+                                inflation_radius,
+                                borders_x,
+                                borders_y,
+                                visualization=True,
+                                save_config=True)
+    
+    generate_random_scenarios(pedestrian_range,
+                                total_scenarios,
+                                robot_vision_range,
+                                inflation_radius,
+                                borders_x,
+                                borders_y,
+                                visualization=True,
+                                save_config=True)
+    
+    generate_parallel_scenarios(pedestrian_range,
+                                total_scenarios,
+                                robot_vision_range,
+                                inflation_radius,
+                                borders_x,
+                                borders_y,
+                                visualization=True,
+                                save_config=True)
+
+
 
 if __name__ == "__main__":
-    main()
+    generate_scenes()
 
---
+++ evaluation/scripts/run_experiment.py
@@ -0,0 +1,168 @@
+from core.controllers import ControllerFactory
+from core.predictors import PredictorFactory, PedestrianTracker
+from core.planners import RandomPlanner, DummyPlanner
+from core.utils import create_sim
+from core.visualizer import Visualizer
+from core.statistics import Statistics
+import numpy as np
+import fire
+import yaml
+import pygame
+import pathlib
+import random
+import torch
+
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+
+pathlib.Path(r"results").mkdir(parents=True, exist_ok=True)
+
+DEFAULT_SCENE_CONFIG_PATH = r"evaluation/scenes/circular_crossing/6/0.yaml"
+DEFAULT_CONTROLLER_CONFIG_PATH = r"evaluation/controllers/MD-MPC-EDC.yaml"
+DEFAULT_RESULT_PATH = r"results/mpc.png"
+
+def main(scene_config_path: str = DEFAULT_SCENE_CONFIG_PATH,
+         controller_config_path: str = DEFAULT_CONTROLLER_CONFIG_PATH,
+         result_path: str = DEFAULT_RESULT_PATH) -> Statistics:
+
+    # Initialization
+    with open(scene_config_path) as f:
+        scene_config = yaml.safe_load(f)
+
+    with open(controller_config_path) as f:
+        controller_config = yaml.safe_load(f)
+
+    config = {}
+    config.update(scene_config)
+    config.update(controller_config)
+
+    simulator, renderer = create_sim(np.array(config["init_state"]),
+                                     config["model_type"],
+                                     config["total_peds"],
+                                     config["is_robot_visible"],
+                                     config["ped_detc_range"],
+                                     np.deg2rad(config["ped_detc_fov"]),
+                                     config["misdetection_prob"],
+                                     config["dt"] / 10,
+                                     config["waypoint_tracker"],
+                                     config["pedestrians_init_states"],
+                                     config["pedestrians_goals"],
+                                     config["ped_model"],
+                                     create_renderer = False)
+    
+    predictor = PredictorFactory.create_predictor(config)
+
+    ped_tracker = PedestrianTracker(predictor, 
+                                    config["max_ghost_tracking_time"], 
+                                    False)
+
+    controller = ControllerFactory.create_controller(config,
+                                                     predictor,
+                                                     ped_tracker)
+
+    statistics = Statistics(simulator,
+                            scene_config_path,
+                            controller_config_path)
+
+    planner = DummyPlanner(global_goal=np.array(config["goal"]),
+                            controller=controller,
+                            subgoal_reach_threshold=config["tolerance_error"],
+                            subgoal_to_goal_threshold=2.,
+                            pedestrian_tracker=ped_tracker,
+                            statistics_module=statistics)
+
+    visualizer = Visualizer(config["total_peds"],
+                            renderer)
+    visualizer.visualize_goal(config["goal"])
+    
+    # Loop
+    simulator.step()
+    hold_time = simulator.sim_dt
+    state = config["init_state"]
+    control = np.array([0, 0]) 
+
+    plot_start_position = False
+
+    while True:
+        # if simulation time exceeded
+        if statistics.simulation_ticks >= 2000:
+            statistics.set_failure_flag()
+            break
+        if renderer:
+            renderer.render()
+        if hold_time >= controller.dt:
+            error = np.linalg.norm(planner.global_goal[:2] - state[:2])
+            if error >= config["tolerance_error"]:
+                state = simulator.current_state.world.robot.state
+                detected_peds_keys = simulator.current_state.sensors["pedestrian_detector"].reading.pedestrians.keys()
+                poses = np.array(list(simulator.current_state.world.pedestrians.poses.values()))[:, :2]
+                vels = np.array(list(simulator.current_state.world.pedestrians.velocities.values()))[:, :2]
+                ground_truth_pedestrians_state = np.concatenate((poses, vels), axis=1)
+                observation = {k: ground_truth_pedestrians_state[k, :] for k in detected_peds_keys}
+                control, predicted_pedestrians_trajectories, predicted_pedestrians_covariances = planner.make_step(state,
+                                                                                                                   observation)
+                predicted_robot_trajectory = controller.get_predicted_robot_trajectory()    
+                statistics.append_current_subgoal(planner.current_subgoal)
+                visualizer.append_ground_truth_robot_state(state)
+                visualizer.append_ground_truth_pedestrians_pose(np.array(list(simulator.current_state.world.pedestrians.poses.values()))[:, :2])
+                visualizer.append_predicted_pedestrians_trajectories(predicted_pedestrians_trajectories[:, :, :2])
+                  
+                visualizer.append_predicted_robot_trajectory(predicted_robot_trajectory)
+                visualizer.append_predicted_pedestrians_covariances(predicted_pedestrians_covariances)    
+                if renderer:
+                    visualizer.visualize_goal(config["goal"])
+                    visualizer.visualize_predicted_robot_trajectory(predicted_robot_trajectory)        
+                    visualizer.visualize_subgoal(planner.current_subgoal)
+                    visualizer.visualize_predicted_pedestrians_trajectory_with_covariances(predicted_pedestrians_trajectories[:, :, :2], predicted_pedestrians_covariances)      
+                hold_time = 0.
+
+                if plot_start_position:
+                    visualizer.plot_final_trajectories(f"Circular Crossing Pattern", 
+                                                       r"results/circular_crossing_starting_position.png", 
+                                                       config)
+                    plot_start_position = False
+            else:
+                if config["experiment_run"]:
+                    break
+                else:
+                    simulator.step(np.array([0, 0]))
+                    print("The goal", *["{0:0.2f}".format(i) for i in controller.goal], "was reached!")
+                    input_value = input("Enter new goal in format '0 0 0' or type 'exit' to exit: ")
+                    if input_value == "exit":
+                        break
+                    new_goal = np.array(list(map(int, input_value.split())))
+                    visualizer.visualize_goal(new_goal)
+                    planner.set_new_global_goal(new_goal)
+
+        simulator.step(control)
+        statistics.track_collisions()
+        statistics.track_simulation_ticks()
+        hold_time += simulator.sim_dt
+
+        if config["experiment_run"] == False:
+            # Terminate the simulation anytime by pressing ESC
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+    pygame.quit()
+
+    statistics._ground_truth_pedestrian_trajectories = visualizer._ground_truth_pedestrian_trajectories
+    statistics._ground_truth_robot_trajectory = visualizer._ground_truth_robot_trajectory
+    statistics._predicted_pedestrians_trajectories = visualizer._predicted_pedestrians_trajectories
+    statistics._predicted_robot_trajectory = visualizer._predicted_robot_trajectory
+    statistics._predicted_pedestrians_covariances = visualizer._predicted_pedestrians_covariances
+
+    if config["experiment_run"] == False:
+        visualizer.make_animation(f"Model Predictive Control", 
+                                result_path, 
+                                config)
+        
+    return statistics
+    
+if __name__ == "__main__":
+    fire.Fire(main)
