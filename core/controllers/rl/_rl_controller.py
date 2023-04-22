1c1abc5c3f65573e25dbe61cf23c194e2f6ade7f
Timur Akhtyamov <rentgeny05@gmail.com>
2023-04-22 20:13:12 +0300

    PPO model v4 added

--- core/controllers/rl/_rl_controller.py
+++ core/controllers/rl/_rl_controller.py
@@ -126,16 +126,50 @@ class RLController(AbstractController):
             "robot": obs_robot[np.newaxis],
             "pred_mean_rl": obs_pred_mean[np.newaxis],
             "pred_cov_rl": obs_pred_cov[np.newaxis],
-            "visibility": peds_vis[np.newaxis]
+            "visibility": peds_vis[np.newaxis],
+        }
+
+    def _build_model_obs_v4(self, robot_pose: np.ndarray, robot_vel: np.ndarray):
+        goal = self.goal[:2]
+
+        rotation_matrix = np.array([[np.cos(robot_pose[2]), -np.sin(robot_pose[2])],
+                                    [np.sin(robot_pose[2]), np.cos(robot_pose[2])]])
+        robot_vel = rotation_matrix @ robot_vel[:2]
+        d_g = np.linalg.norm(robot_pose[:2] - goal)
+        phi_goal = wrap_angle(robot_pose[2] - np.arctan2(goal[1] - robot_pose[1], goal[0] - robot_pose[0]))
+        obs_robot = np.array([d_g, phi_goal, robot_vel[0], robot_vel[1], ROBOT_RADIUS])
+
+        current_poses = self._ped_tracker.get_current_poses()
+        pred_mean = np.tile(np.array([-10., -10.]), (self._peds_padding, 8, 1))
+        pred_cov = np.tile(np.eye(2) * 0.001, (self._peds_padding, 7, 1, 1))
+        pred_vis = np.zeros(self._peds_padding, dtype=np.bool)
+
+        for i, (k, v) in enumerate(self._ped_tracker.get_predictions().items()):
+            pred_mean[i, :, :] = np.concatenate((current_poses[k][np.newaxis], v[0][:7]), axis=0) - robot_pose[:2]
+            pred_cov[i] = v[1][:7]
+            pred_vis[i] = True
+
+        if pred_vis.any():
+            pred_mean[pred_vis] = np.einsum("ij,mnj->mni", rotation_matrix, pred_mean[pred_vis])
+            pred_cov[pred_vis] = np.einsum("ij,mnjk->mnik", rotation_matrix, pred_cov[pred_vis])
+            pred_cov[pred_vis] = np.einsum("mnij,jk->mnik", pred_cov[pred_vis], rotation_matrix.T)
+
+        return {
+            "robot": obs_robot[np.newaxis],
+            "pred_mean_rl": pred_mean[np.newaxis],
+            "pred_cov_rl": pred_cov[np.newaxis],
+            "visibility": pred_vis[np.newaxis],
         }
 
 
     def make_step(self, state: np.ndarray, observation: Dict[int, np.ndarray], robot_velocity: np.ndarray) -> np.ndarray:
         self._predicted_pedestrians_trajectories, self._predicted_pedestrians_covariances = self._get_output_predictions()
-        if self._model_id != "ppo_model_v3":
-            obs = self._build_model_obs(state, robot_velocity)
-        else:
+        if self._model_id == "ppo_model_v3":
             obs = self._build_model_obs_v3(state, robot_velocity)
+        elif self._model_id == "ppo_model_v4":
+            obs = self._build_model_obs_v4(state, robot_velocity)
+        else:
+            obs = self._build_model_obs(state, robot_velocity)
 
         with torch.no_grad():
             actions, states = self._rl_model.predict(obs, self._states, deterministic=True)
@@ -143,7 +177,6 @@ class RLController(AbstractController):
             actions = unnormalize_symmetric(actions,
                                             lb=np.array([0., -2.]),
                                             ub=np.array([2., 2.]))
-            print(actions)
             self._states = states
 
         return actions, self._predicted_pedestrians_trajectories, self._predicted_pedestrians_covariances
